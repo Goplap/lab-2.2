6eb7b979b9e4388aaa0c749391e83c106ebffb86
Goplap <alexey.antsybor@gmail.com>
2025-04-28 16:19:29 +0300

    Dto

---
+++ DAL/Models/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BulletinBoard.BLL.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
